52555ce49c41dc3cfcdc692c2edba8b8886a53d6
Laurentiu Cristian Ion <ionlaurentiucristian@gmail.com>
2012-11-14 22:30:48 +0200

    install setuptools if non existent

--- install.sh
+++ install.sh
@@ -3,8 +3,8 @@ clear
 chmod +x main.py
 chmod +x Tank-AI.exe
 echo "Installing Tank-AI dependencies"
+sudo apt-get install build-essential python-dev swig python-setuptools
 sudo easy_install pygame pygsear
-sudo apt-get install build-essential python-dev swig
 cd Box2D-2.0.2b2
 python setup.py build
 sudo python setup.py install
